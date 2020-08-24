0b049eb8328981decca6b7f48611f894f9d74ad5
riyasavant <rieee2110@gmail.com>
2020-08-24 22:45:30 +0530

    Corner cases tested, refcatoring needed

--- Main.java
+++ Main.java
@@ -1,9 +1,66 @@
+import java.util.Scanner;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		System.out.println("Hello, Java!");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		//Input Details of player 1
+		System.out.print("Player 1 name: ");
+		String name = scanner.next();
+		System.out.print("Player 1, X or O: ");
+		
+		//To input a character
+		char option = scanner.next().charAt(0);
 
+		Person player1 = new Person(name, option);
+		
+		//Input details of player 2
+		System.out.print("Player 2 name: ");
+		name = scanner.next();
+
+		if(option == 'X') {
+			option = 'O';
+		} else {
+			option = 'X';
+		}
+		
+		Person player2 = new Person(name, option);
+		
+		player1.printData();
+		player2.printData();
+		
+		Grid grid = new Grid();
+		grid.printGrid();
+		boolean result;
+		int counter = 1;
+		
+		while(true) {
+			System.out.print(player1.name + ", " + player1.option + " at position: ");
+			result = grid.input(player1.option, scanner.nextInt());
+			if(result == true) {
+				System.out.print(player1.name + " wins!");
+				break;
+			} else {
+				grid.printGrid();
+				if(counter == 9) {
+					System.out.print("\n Match Drawn");
+					break;
+				} else {
+					System.out.print(player2.name + ", " + player2.option + " at position: ");
+					result = grid.input(player2.option, scanner.nextInt());
+					if(result == true) {
+						System.out.print(player2.name + " wins!");
+						break;
+					} else {
+						grid.printGrid();
+					}
+				}
+			}
+			counter += 2;
+		}
+		
 	}
-
+	
 }
